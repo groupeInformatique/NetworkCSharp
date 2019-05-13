ce2d082907d75201312df9cf66f1d588778705d7
Xavier Lafontaine <xavier.lafontaine777@gmail.com>
2019-05-13 09:25:33 -0400

    gogogo

---
+++ Firewall.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NetFwTypeLib;
+
+namespace wFirewall
+{
+    public class wFirewall
+    {
+        public wFirewall()
+        {
+            INetFwMgr icfMgr = null;
+            try
+            {
+                Type TicfMgr = Type.GetTypeFromProgID("HNetCfg.FwMgr");
+                icfMgr = (INetFwMgr)Activator.CreateInstance(TicfMgr);
+            }
+            catch (Exception ex)
+            {
+                return;
+            }
+
+            try
+            {
+                INetFwProfile profile;
+                INetFwOpenPort portClass;
+                Type TportClass = Type.GetTypeFromProgID("HNetCfg.FWOpenPort");
+                portClass = (INetFwOpenPort)Activator.CreateInstance(TportClass);
+
+                // Get the current profile
+                profile = icfMgr.LocalPolicy.CurrentProfile;
+
+                // Set the port properties
+                portClass.Scope = NetFwTypeLib.NET_FW_SCOPE_.NET_FW_SCOPE_ALL;
+                portClass.Enabled = true;
+                portClass.Protocol = NetFwTypeLib.NET_FW_IP_PROTOCOL_.NET_FW_IP_PROTOCOL_TCP;
+                portClass.Name = "DescriptionOfRule";
+                portClass.Port = 8000;
+
+                // Add the port to the ICF Permissions List
+                profile.GloballyOpenPorts.Add(portClass);
+                return;
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+    }
+}
